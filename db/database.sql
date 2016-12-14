c61de67d2cf4ec795f0015a208d594c2fe06a8d5
Geri <gergo.turcsanyi@avatao.com>
2016-12-14 13:21:26 +0100

    Init

---
+++ db/database.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE not_important_table (id int, name varchar(500));
+INSERT INTO not_important_table VALUES (1, 'test');
+
+CREATE TABLE credit_card (id int, cc_number bigint, cvv integer);
+INSERT INTO credit_card VALUES (1, 1111222233334444, 321);
+INSERT INTO credit_card VALUES (2, 1234567890123245, 963);
